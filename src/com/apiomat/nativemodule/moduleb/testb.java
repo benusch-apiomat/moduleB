224a77c32e0a0bbf8278a7ddcdf0cc28d546a9a6
WIN-4O8SPPBNG6C$ <WIN-4O8SPPBNG6C$@WIN-4O8SPPBNG6C.apiomat.local>
2016-10-05 10:39:10 +0200

    [AOM] Change MetaModel

---
+++ src/com/apiomat/nativemodule/moduleb/testb.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright (c) 2011 - 2016, Apinauten GmbH
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.apiomat.nativemodule.moduleb;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+import com.apiomat.nativemodule.*;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+
+
+import com.apiomat.nativemodule.basics.*;
+import com.apiomat.nativemodule.AuthState;
+/**
+* Generated class for your testb data model
+*
+* DO NOT CHANGE ANY CODE EXCEPT CLASS ANNOTATIONS OR CLASS ATTRIBUTES HERE!
+* EVERYTHING ELSE WILL GET OVERWRITTEN!
+*
+*/
+@SuppressWarnings( "unused" )
+@Model( moduleName = "ModuleB",
+    hooksClassNameTransient = "com.apiomat.nativemodule.moduleb.testbHooksTransient", 
+    hooksClassNameNonTransient = "com.apiomat.nativemodule.moduleb.testbHooksNonTransient", 
+            isTransient = false,     requiredUserRoleCreate=UserRole.User, requiredUserRoleRead=UserRole.User,
+    requiredUserRoleWrite=UserRole.Owner, restrictResourceAccess=false,
+    allowedRolesCreate={}, allowedRolesRead={},
+    allowedRolesWrite={}, allowedRolesGrant={})
+public class testb extends AbstractClientDataModel implements IModel<testb>
+{
+    /**
+     * Contains the name of the module that this model belongs to
+     */
+    public static final String MODULE_NAME = "ModuleB";
+    /**
+     * Contains the name of the model
+     */
+    public static final String MODEL_NAME = "testb";
+
+    /** class specific attributes */
+    /**
+     * Protected constructor; to create a new instance, use the createObject() method
+     */
+    public testb ()
+    {}
+
+    /**
+     * Returns the name of the module where this class belongs to
+     */
+    @Override
+    public String getModuleName( )
+    {
+        return MODULE_NAME;
+    }
+
+    /**
+     * Returns the name of the model
+     */
+    @Override
+    public String getModelName( )
+    {
+        return MODEL_NAME;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public void write( final Kryo kryo, final Output output )
+    {
+        super.write( kryo, output );
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public void read( final Kryo kryo, final Input input )
+    {
+        super.read( kryo, input );
+
+        final Request req = (Request)kryo.getContext( ).get( "creq" );
+        req.toString( );
+    }
+}
